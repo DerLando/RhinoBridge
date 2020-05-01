90d7c3501b8f500e871df6be4e80549e6097264a
Lando <lando.schumpich@gmail.com>
2020-05-01 19:09:50 +0200

    update to R7

--- RhinoBridge/Converters/AssetConverter.cs
+++ RhinoBridge/Converters/AssetConverter.cs
@@ -8,6 +8,8 @@ using bridge_c_sharp_plugin;
 using Rhino;
 using Rhino.DocObjects;
 using Rhino.Render;
+using RhinoBridge.Data;
+using Texture = bridge_c_sharp_plugin.Texture;
 
 namespace RhinoBridge.Converters
 {
@@ -27,33 +29,38 @@ namespace RhinoBridge.Converters
                 Name = asset.name
             };
 
+            var pbr = mat.ConvertToPhysicallyBased();
 
             // this could be nicer with pbr materials in rhino 7
             foreach (var texture in asset.textures)
             {
-                switch (GetTextureType(texture))
-                {
-                    case TextureType.Albedo:
-                        mat.SetBitmapTexture(texture.path);
-                        break;
-                    case TextureType.AmbientOcclusion:
-                        break;
-                    case TextureType.Cavity:
-                        break;
-                    case TextureType.Displacement:
-                        break;
-                    case TextureType.Gloss:
-                        break;
-                    case TextureType.Normal:
-                        mat.SetBumpTexture(texture.path);
-                        break;
-                    case TextureType.Roughness:
-                        break;
-                    case TextureType.Specular:
-                        break;
-                    case TextureType.Undefined:
-                        break;
-                }
+                //switch (GetTextureType(texture))
+                //{
+                //    case TextureType.Albedo:
+                //        mat.SetBitmapTexture(texture.path);
+                //        break;
+                //    case TextureType.AmbientOcclusion:
+                //        break;
+                //    case TextureType.Cavity:
+                //        break;
+                //    case TextureType.Displacement:
+                //        break;
+                //    case TextureType.Gloss:
+                //        break;
+                //    case TextureType.Normal:
+                //        mat.SetBumpTexture(texture.path);
+                //        break;
+                //    case TextureType.Roughness:
+                //        break;
+                //    case TextureType.Specular:
+                //        break;
+                //    case TextureType.Undefined:
+                //        break;
+                //}
+                var convertedTexture = Convert(texture);
+
+                pbr.SetTexture(convertedTexture, convertedTexture.TextureType);
+                mat.SetTexture(convertedTexture, convertedTexture.TextureType);
             }
 
             mat.DiffuseColor = GetAverageColor(asset);
@@ -79,6 +86,49 @@ namespace RhinoBridge.Converters
             if (texture.type == "specular") return TextureType.Specular;
 
             return TextureType.Undefined;
+        }
+
+        public static Rhino.DocObjects.TextureType ExtractType(Texture texture)
+        {
+            if (texture.type == "albedo") return Rhino.DocObjects.TextureType.PBR_BaseColor;
+            if (texture.type == "ao") return Rhino.DocObjects.TextureType.PBR_AmbientOcclusion;
+            if (texture.type == "cavity") return Rhino.DocObjects.TextureType.PBR_ClearcoatBump;
+            if (texture.type == "displacement") return Rhino.DocObjects.TextureType.PBR_Displacement;
+            if (texture.type == "gloss") return Rhino.DocObjects.TextureType.PBR_Clearcoat;
+            if (texture.type == "normal") return Rhino.DocObjects.TextureType.Bump;
+            if (texture.type == "roughness") return Rhino.DocObjects.TextureType.PBR_Roughness;
+            if (texture.type == "specular") return Rhino.DocObjects.TextureType.PBR_Specular;
+
+            return Rhino.DocObjects.TextureType.None;
+        }
+
+        /// <summary>
+        /// Extracts the texture information of a given texture
+        /// </summary>
+        /// <param name="texture"></param>
+        /// <returns></returns>
+        public static TextureInformation ExtractInformation(Texture texture)
+        {
+            return new TextureInformation(texture.path, ExtractType(texture));
+        }
+
+        /// <summary>
+        /// Creates a Rhino Texture from a TextureInformation
+        /// </summary>
+        /// <param name="information"></param>
+        /// <returns></returns>
+        public static Rhino.DocObjects.Texture FromInformation(in TextureInformation information)
+        {
+            return new Rhino.DocObjects.Texture
+            {
+                FileName = information.FilePath,
+                TextureType = information.Type
+            };
+        }
+
+        public static Rhino.DocObjects.Texture Convert(Texture texture)
+        {
+            return FromInformation(ExtractInformation(texture));
         }
 
         /// <summary>
---
+++ RhinoBridge/Data/TextureInformation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino.DocObjects;
+
+namespace RhinoBridge.Data
+{
+    /// <summary>
+    /// Texture information
+    /// </summary>
+    public readonly struct TextureInformation
+    {
+        /// <summary>
+        /// The file path of the texture image of this texture
+        /// </summary>
+        public readonly string FilePath;
+
+        /// <summary>
+        /// The Type as defined inside of Rhinocommon
+        /// </summary>
+        public readonly TextureType Type;
+
+        public TextureInformation(string filePath, TextureType type)
+        {
+            FilePath = filePath;
+            Type = type;
+        }
+    }
+}
